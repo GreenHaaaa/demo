95fe6b6bb432168fad2d1743b6c5c397e12cb173
刘林杰 <1335033346@qq.com>
2018-04-12 11:26:10 +0800

    添加文件上传审核

--- src/main/java/com/example/arcface/controller/TaskController.java
+++ src/main/java/com/example/arcface/controller/TaskController.java
@@ -94,7 +94,9 @@ public class TaskController {
         if(user.getRole().equals(ADMIN_ROLE)){
             if(isOk==1){
                 FileWalkUtil.copyFile(TEMPPATH+"/"+id+"/"+filename,sourcePath+"/"+id+"/"+filename);
+                return new ResponseEntity<>(new Info("success",200),HttpStatus.OK);
             }
+            return new ResponseEntity<>(new Info("Althrough you not permit it ",200),HttpStatus.OK);
         }
         return new ResponseEntity<>(new Info("you are not admin",403),HttpStatus.FORBIDDEN);
     }
